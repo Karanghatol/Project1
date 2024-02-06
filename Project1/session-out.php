99c43ec3beaa7975b2bd36f4b99c766e4da333bf
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2024-02-06 23:02:49 +0530

    Add files via upload

---
+++ Project1/session-out.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center style="margin-top: 100px;">
+<h1>SESSION_OUT</h1>
+<h3>please login.</h3>
+        <a href="admin-doctor.php">Go to Login</a>
+    </center>
+</body>
+</html>
